960fd48519023794e83a16d04ef109dad70caf55
faith0disandro <fdisandr@ucsd.edu>
2022-12-26 17:59:41 -0800

    init

--- middlewares/admin.js
+++ middlewares/admin.js
@@ -20,7 +20,7 @@ const admin = async (req, res, next) => {
     req.token = token;
     next();
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    res.status(500).json({ error: err.message })
   }
 };
 
